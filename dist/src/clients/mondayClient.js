1d3cb7cb2ac099e251938835bda3d1d490fb1ddf
John Mervin Bocatcat <johnmervin.bocatcat@888holdings.com>
2024-12-04 15:58:37 +0800

    Cleanup

--- dist/src/clients/mondayClient.js
+++ dist/src/clients/mondayClient.js
@@ -1,7 +1,8 @@
-import { createMondayClient } from "monstaa";
-export const mondayClient = createMondayClient({
-    name: "tuesday",
-    version: '23-10',
-    apiToken: "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE3ODQwNDMyMiwidWlkIjozMzgyMTg5MiwiaWFkIjoiMjAyMi0wOC0zMVQxMTo0OTowMC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTExNTEwMSwicmduIjoidXNlMSJ9.I4m4CXxhai3k4JumWdc0NO_5EiSjfhE0rxF5_njzJA0",
+import { ApiClient } from "@mondaydotcomorg/api";
+export const mondayClient = new ApiClient({
+    requestConfig: {
+        errorPolicy: 'all'
+    },
+    token: "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE3ODQwNDMyMiwidWlkIjozMzgyMTg5MiwiaWFkIjoiMjAyMi0wOC0zMVQxMTo0OTowMC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTExNTEwMSwicmduIjoidXNlMSJ9.I4m4CXxhai3k4JumWdc0NO_5EiSjfhE0rxF5_njzJA0",
 });
 //# sourceMappingURL=mondayClient.js.map
--- src/clients/mondayClient.ts
+++ src/clients/mondayClient.ts
@@ -1,8 +1,9 @@
-import { createMondayClient } from "monstaa";
+import { ApiClient } from "@mondaydotcomorg/api";
 
-export const mondayClient = createMondayClient({
-  name: "tuesday",
-  version: '23-10',
-  apiToken:
+export const mondayClient = new ApiClient({
+  requestConfig: {
+    errorPolicy: 'all'
+  },
+  token:
     "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE3ODQwNDMyMiwidWlkIjozMzgyMTg5MiwiaWFkIjoiMjAyMi0wOC0zMVQxMTo0OTowMC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTExNTEwMSwicmduIjoidXNlMSJ9.I4m4CXxhai3k4JumWdc0NO_5EiSjfhE0rxF5_njzJA0",
 });
