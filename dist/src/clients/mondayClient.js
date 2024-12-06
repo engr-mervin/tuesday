f63ff1a1743f559e874ae29d158aec4328a889e0
John Mervin Bocatcat <johnmervin.bocatcat@888holdings.com>
2024-12-07 02:16:47 +0800

    cp

--- dist/src/clients/mondayClient.js
+++ dist/src/clients/mondayClient.js
@@ -1,6 +1,7 @@
-import mondaySdk from "monday-sdk-js";
-const mondayClient = mondaySdk();
-mondayClient.setApiVersion('2023-10');
-mondayClient.setToken("eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE3ODQwNDMyMiwidWlkIjozMzgyMTg5MiwiaWFkIjoiMjAyMi0wOC0zMVQxMTo0OTowMC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTExNTEwMSwicmduIjoidXNlMSJ9.I4m4CXxhai3k4JumWdc0NO_5EiSjfhE0rxF5_njzJA0");
-export { mondayClient };
+import { createMondayClient } from "monstaa";
+export const mondayClient = createMondayClient({
+    name: "tuesday",
+    version: '23-10',
+    apiToken: "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE3ODQwNDMyMiwidWlkIjozMzgyMTg5MiwiaWFkIjoiMjAyMi0wOC0zMVQxMTo0OTowMC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTExNTEwMSwicmduIjoidXNlMSJ9.I4m4CXxhai3k4JumWdc0NO_5EiSjfhE0rxF5_njzJA0",
+});
 //# sourceMappingURL=mondayClient.js.map
--- src/clients/mondayClient.ts
+++ src/clients/mondayClient.ts
@@ -1,8 +1,8 @@
-import mondaySdk from "monday-sdk-js";
+import { createMondayClient } from "monstaa";
 
-const mondayClient = mondaySdk();
-mondayClient.setApiVersion('2023-10');
-mondayClient.setToken("eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE3ODQwNDMyMiwidWlkIjozMzgyMTg5MiwiaWFkIjoiMjAyMi0wOC0zMVQxMTo0OTowMC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTExNTEwMSwicmduIjoidXNlMSJ9.I4m4CXxhai3k4JumWdc0NO_5EiSjfhE0rxF5_njzJA0");
-
-
-export { mondayClient };
+export const mondayClient = createMondayClient({
+  name: "tuesday",
+  version: '23-10',
+  apiToken:
+    "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE3ODQwNDMyMiwidWlkIjozMzgyMTg5MiwiaWFkIjoiMjAyMi0wOC0zMVQxMTo0OTowMC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTExNTEwMSwicmduIjoidXNlMSJ9.I4m4CXxhai3k4JumWdc0NO_5EiSjfhE0rxF5_njzJA0",
+});
